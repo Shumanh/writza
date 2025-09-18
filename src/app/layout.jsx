2f6645f0fcac3cf6103d93bdb7c90349d698b205
ashyuman <theshumanhere@gmail.com>
2025-09-18 11:05:45 +0545

    fixed the scrolling

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "@/styles/twitter-card-fix.css";
 import "@/styles/prosemirror.css";
 // import "katex/dist/katex.min.css"; // Removed to avoid CSS ESM loader error
 
@@ -8,7 +9,11 @@ import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${playfair.variable} ${inter.variable} ${cal.variable}`} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${montserrat.variable} ${playfair.variable} ${inter.variable} ${cal.variable}`}
+      suppressHydrationWarning
+    >
       <body className="font-default bg-white min-h-screen flex flex-col" suppressHydrationWarning>
         {/* Globally load Twitter widgets to ensure availability on blog pages */}
         <Script src="https://platform.twitter.com/widgets.js" strategy="afterInteractive" />
